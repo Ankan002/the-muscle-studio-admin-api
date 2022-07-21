5ab99a7c07eead8c8acb45b8047b125bd46f2a7f
ankanbhattacharya <ankanbhattachatya89@gmail.com>
2022-07-21 09:51:18 +0530

    chore: :wrench: initial build

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,26 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser";
 import expressFileUpload from "express-fileupload";
 import { ApolloServer } from "apollo-server-express";
 import { welcomeTypeDefs } from "graphql/typeDefs";
 import { welcomeResolver } from "graphql/resolvers";
+import cookieParser from "cookie-parser";
+
+import { welcomeRouter } from "routes/welcome";
+import { origins } from "constants/cors-origins";
 
 const startServer = async () => {
 	const app = express();
-	const PORT = process.env.PORT;
+	const PORT = process.env["PORT"];
 
 	app.use(
 		cors({
-			origin: "*",
 			credentials: true,
+			origin: origins,
 		})
 	);
-	app.use(cookieParser());
 	app.use(express.json());
+	app.use(cookieParser());
 	app.use(
 		expressFileUpload({
 			useTempFiles: true,
@@ -28,7 +31,7 @@ const startServer = async () => {
 		typeDefs: [welcomeTypeDefs],
 		resolvers: [welcomeResolver],
 		context: (context) => {
-			const authToken = context?.req?.cookies?.authToken;
+			const authToken = context.req.cookies?.authToken;
 			return {
 				authToken,
 			};
@@ -40,11 +43,14 @@ const startServer = async () => {
 	apolloServer.applyMiddleware({ app });
 
 	app.get("/", (req: Request, res: Response) => {
+		console.log(req.user);
 		res.status(200).json({
 			success: true,
-			message: "Welcome to The Muscle Studio admin API",
+			message: "Welcome to gym management shop API",
 		});
 	});
+
+	app.use("/api", welcomeRouter);
 
 	app.listen(PORT, () => console.log(`App is running at Port: ${PORT}`));
 };
